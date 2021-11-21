fa4c7154157779cd6e1009741886ba30631c55c5
maria.dragilev <mariya1.dr@gmail.com>
2021-11-21 20:09:04 +0200

    ''

--- java.js
+++ java.js
@@ -20,18 +20,30 @@ function numbers() {
         var intr = setInterval(function() {
             document.getElementById("Ny").innerText=String(NumCinY);
             NumCinY=NumCinY+125;
-            if (NumCinY > 8001) clearInterval(intr);
+            if (NumCinY > 8001) {
+                clearInterval(intr);
+                document.getElementById("Ny").classList+=" animate__animated animate__pulse";
+                console.log( document.getElementById("Ny").classList);
+              }
           }, 30)
           
         var intrK = setInterval(function() {
             document.getElementById("Nk").innerText=String(NumCinK);
             NumCinK=NumCinK+10;
-            if (NumCinK > 1800) clearInterval(intrK);
-          }, 10)
+            if (NumCinK > 1800) {
+              clearInterval(intrK);
+              document.getElementById("Nk").classList+=" animate__animated animate__pulse";
+
+            }
+          }, 11)
 
         var intrEX = setInterval(function() {
             document.getElementById("Exy").innerText=String(NumExist);
             NumExist=NumExist+1;
-            if (NumExist > 59) clearInterval(intrEX);
-          }, 30)
+            if (NumExist > 59) {
+              clearInterval(intrEX);
+              document.getElementById("Exy").classList+=" animate__animated animate__pulse";
+
+            }
+          }, 35)
 }
