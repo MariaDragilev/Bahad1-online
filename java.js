6da48dac5dec63d6c43f0ea2785a7afe65f87b93
maria.dragilev <mariya1.dr@gmail.com>
2021-11-21 19:12:24 +0200

    new

--- java.js
+++ java.js
@@ -21,17 +21,17 @@ function numbers() {
             document.getElementById("Ny").innerText=String(NumCinY);
             NumCinY=NumCinY+125;
             if (NumCinY > 8001) clearInterval(intr);
-          }, 25)
+          }, 30)
           
         var intrK = setInterval(function() {
             document.getElementById("Nk").innerText=String(NumCinK);
             NumCinK=NumCinK+10;
             if (NumCinK > 1800) clearInterval(intrK);
-          }, 1)
+          }, 30)
 
         var intrEX = setInterval(function() {
             document.getElementById("Exy").innerText=String(NumExist);
             NumExist=NumExist+1;
             if (NumExist > 59) clearInterval(intrEX);
-          }, 1)
+          }, 30)
 }
