3267146ad844ba1a32f078d164a256b40e148e11
maria.dragilev <mariya1.dr@gmail.com>
2021-11-22 12:15:29 +0200

    navbar

--- java.js
+++ java.js
@@ -1,13 +1,50 @@
-var NumCinK=0, NumCinY=0, NumExist=0;
+var NumCinK=0, NumCinY=0, NumExist=0, menuOpen=Boolean(false);
 
 onLoad();
 function onLoad() {
     // document.getElementById("btn").addEventListener("click",onclickf);
+    document.getElementById("Humburger").addEventListener("click",clickhamb);
     setTimeout(() => {
         
         numbers();
     }, 1000);
     
+}
+
+function clickhamb(event) {
+  if(menuOpen == false) {
+    console.log("open hukj");
+    openHamborger();
+  }
+  else {
+    console.log("else")
+    closeHamburger();
+  }
+}
+
+function openHamborger(){
+  document.getElementById("menu").style.opacity="1";
+
+  for ( var i = 0; i < document.getElementsByClassName("gdudim").length; i++) {
+    document.getElementsByClassName("gdudim")[i].style.height="2em";
+    document.getElementsByClassName("gdudim")[i].style.fontSize="1.5em";
+
+  }
+  menuOpen=true;
+}
+
+function closeHamburger() {
+  for ( var i = 0; i < document.getElementsByClassName("gdudim").length; i++) {
+    document.getElementsByClassName("gdudim")[i].style.height="0";
+    document.getElementsByClassName("gdudim")[i].style.fontSize="0";
+    
+  }
+  menuOpen=false;
+  setTimeout(() => {
+    
+    document.getElementById("menu").style.opacity="0";
+  }, 800);
+
 }
 
 function onclickf(event) {
